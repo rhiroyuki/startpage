388e80cd9ac378e93a236c9650f819001d6526cc
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-10-18 00:40:57 -0200

    Add gulp clean css and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var gulp = require('gulp');
+let gulp = require('gulp');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
+let sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function() {
+  gulp.src('./scss/**/*.scss')
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(cleanCSS({ level: 2 }))
+  .pipe(gulp.dest('./css'));
 });
